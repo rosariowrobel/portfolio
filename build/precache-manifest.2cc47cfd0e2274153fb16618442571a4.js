2dfe8f4f96d88c7d89cf0e253d44d7ea9d33f201
Rosario <rosario.wrobel@gmail.com>
2020-09-13 22:48:44 -0300

    deploy

---
+++ build/precache-manifest.2cc47cfd0e2274153fb16618442571a4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56854dbc3844a956c1e3b4d0190b5b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d80cbe9f5f2834d7fea",
+    "url": "/static/css/2.0b794cb9.chunk.css"
+  },
+  {
+    "revision": "d35c49ed2dbfad83d194",
+    "url": "/static/css/main.cf91659d.chunk.css"
+  },
+  {
+    "revision": "2d80cbe9f5f2834d7fea",
+    "url": "/static/js/2.43a7290b.chunk.js"
+  },
+  {
+    "revision": "51972e78d6fc143a08609a32e596661b",
+    "url": "/static/js/2.43a7290b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35c49ed2dbfad83d194",
+    "url": "/static/js/main.75d6e05f.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "03f644ffdc642fa9a56cb4f1e7932f99",
+    "url": "/static/media/familia.03f644ff.png"
+  },
+  {
+    "revision": "e11c64edc0c0d19dc7a59725eca8232a",
+    "url": "/static/media/fondo.e11c64ed.jpg"
+  },
+  {
+    "revision": "827c21a8e7b3072cd1d57494de583416",
+    "url": "/static/media/fondoCon.827c21a8.png"
+  },
+  {
+    "revision": "cdf5dcab3863bbdc66550957b44eeb59",
+    "url": "/static/media/image1.cdf5dcab.jpg"
+  },
+  {
+    "revision": "455b3e02afd8756c698a27d352e0d759",
+    "url": "/static/media/image2.455b3e02.jpg"
+  },
+  {
+    "revision": "9b76d0d4266291c53147291ab6845a4f",
+    "url": "/static/media/image3.9b76d0d4.jpg"
+  },
+  {
+    "revision": "5698dafa75ce781d0eacbf727fbb0a57",
+    "url": "/static/media/image4.5698dafa.jpg"
+  },
+  {
+    "revision": "ce36e09c85bf02dc23ffd43cda270a0b",
+    "url": "/static/media/image5.ce36e09c.jpg"
+  },
+  {
+    "revision": "3221bb9fb5ac49614db1328af23c5a8e",
+    "url": "/static/media/image6.3221bb9f.jpg"
+  },
+  {
+    "revision": "303c44f1ba6066cd1ad844fd6ee60a39",
+    "url": "/static/media/imagecel1.303c44f1.png"
+  },
+  {
+    "revision": "5f2f251858edb1e3a00812f837ff7bf2",
+    "url": "/static/media/imagecel2.5f2f2518.png"
+  },
+  {
+    "revision": "57317a61f15029a21f67e6d36e0768e0",
+    "url": "/static/media/imagecel3.57317a61.png"
+  },
+  {
+    "revision": "b0a778090f0714d9364419c7a68ba97a",
+    "url": "/static/media/imagecel5.b0a77809.png"
+  },
+  {
+    "revision": "0cba602cca4777be0c46057f0561ce70",
+    "url": "/static/media/imagecel6.0cba602c.png"
+  }
+]);
